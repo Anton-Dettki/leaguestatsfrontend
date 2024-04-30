9460fa084ed3481ec596b4ec9dd85ac9943e99ca
carrotFarmer78 <caspardett@gmail.com>
2024-04-30 13:17:47 +0200

    Added fully functional footer

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@
   <v-main app>
     <v-container>
       <Home v-if="currentTab === 'Home'"/>
+      <About v-if="currentTab === 'About'"/>
+      <Contact v-if="currentTab === 'Contact'"/>
     </v-container>
   </v-main>
 
-    <Footer></Footer>
+    <Footer @tab-changed="switchTab"></Footer>
 
 
 </v-app>
@@ -18,13 +20,15 @@
 import { ref } from "vue";
 
 import Header from "@/components/Header.vue";
-import Home from "@/views/Home.vue";
 import Footer from "@/components/Footer.vue";
+
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Contact from "@/views/Contact.vue";
 
 const currentTab = ref("Home")
 
 function switchTab(tab) {
-  console.log(tab)
   currentTab.value = tab
 }
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,17 @@
 <template>
   <v-footer app>
 
-    <v-btn variant="plain" href="https://github.com/carrotFarmer78">
+    <v-btn href="https://github.com/carrotFarmer78">
       <v-icon icon="fab fa-github"/>
     </v-btn>
-    <v-btn variant="plain" href="https://github.com/carrotFarmer78">
-      <v-icon icon="fab fa-github"/>
+    <v-btn
+        variant="plain"
+        v-for="(_, tab) in tabs"
+        :key="tab"
+        :class="['tab-button', { active: currentTab === tab }]"
+        @click="setCurrentTab(tab)"
+    >
+      {{ tab }}
     </v-btn>
 
 
@@ -16,8 +22,34 @@
 
 <script setup>
 
+import {defineEmits, shallowRef} from "vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Contact from "@/views/Contact.vue";
+
+const currentTab = shallowRef("Home")
+
+const tabs = {
+  'Home': String,
+  'About': String,
+  'Contact': String
+}
+
+
+const emit = defineEmits(['tab-changed'])
+
+function setCurrentTab(tab) {
+  currentTab.value = tab
+  emit('tab-changed', tab)
+}
+
 </script>
 
 <style scoped>
+.tab-button {
+  margin: 0 10px;
+  padding: 10px;
+  cursor: pointer;
+}
 
 </style>
